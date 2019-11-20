7da89a495f38026f3efcee668fd6164603583623
Danny Lee <dreamer@panix.com>
2019-11-20 17:35:10 -0500

    mine

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,38 +1,50 @@
 import React from 'react'
 
-const MenuBar = (props) => {
-
+// const MenuBar = (props) => {
+const MenuBar = ({changeSelected, selected}) => {
+  // console.log({changeSelected, selected})
   /*
-
   The 'a' tags below are the menu items. Think about the way a menu 
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
   on a menu item? Do we need state in this component, and if not, how can
   this component be made aware of what is currently the active menu item?
-
   */
+    const onClickHandler = function(event) {
+      changeSelected(event.target.id)
+      props.changeSelected
+    }
 
-  return (
-    <div className="ui four item menu">
-      <a className="item active" id="profile">
-        <i className="user large icon" id="profile"/>
-      </a>
+    return (
+      <div className="ui four item menu">
+        <a  onClick={onClickHandler} 
+            className={"item " + (selected==="profile" ? "active" : "null")}
+            id="profile">
+          <i className="user large icon" id="profile"/>
+        </a>
 
-      <a className="item" id="photo">
-        <i className="photo large icon" id="photo"/>
-      </a>
+        <a  onClick={onClickHandler} 
+            className={"item " + (selected==="photo" ? "active" : "null")}
+            id="photo">
+          <i className="photo large icon" id="photo"/>
+        </a>
 
-      <a className="item" id="cocktail">
-        <i className="cocktail large icon" id="cocktail"/>
-      </a>
+        <a  onClick={onClickHandler} 
+            className={"item " + (selected==="cocktail" ? "active" : "null")}
+            id="cocktail">
+          <i className="cocktail large icon" id="cocktail"/>
+        </a>
 
-      <a className="item" id="pokemon"> 
-        <i className=" themeisle large icon" id="pokemon"/>
-      </a>
-    </div>
-  )
+        <a  onClick={onClickHandler} 
+            className={"item " + (selected==="pokemon" ? "active" : "null")}
+            id="pokemon"> 
+          <i className=" themeisle large icon" id="pokemon"/>
+        </a>
+      </div>
+    )
 
-}
+  }
+
 
 export default MenuBar
